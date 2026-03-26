9318db7d83d0d7128c156a4ac6cccf48ed36e4ac
ramalakshmi8 <ramalakshumma482@gmail.com>
2026-03-26 20:03:04 +0530

    Update get-by-slug route to remove isAuth middleware
    Removed authentication middleware from the get-by-slug route.

--- server/routes/website.route.js
+++ server/routes/website.route.js
@@ -15,6 +15,6 @@ websiteRouter.get("/get-by-id/:id", isAuth, getWebsiteById);
 websiteRouter.get("/get-all", isAuth, getAll);
 websiteRouter.post("/update/:id", isAuth, changes);
 websiteRouter.get("/deploy/:id", isAuth, deploy);
-websiteRouter.get("/get-by-slug/:slug", isAuth, getBySlug);
+websiteRouter.get("/get-by-slug/:slug",getBySlug);
 
 export default websiteRouter;
