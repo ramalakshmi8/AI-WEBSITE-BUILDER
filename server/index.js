0c89907e44540ccf5912a3297fc8985e719637a7
ramalakshmi8 <ramalakshumma482@gmail.com>
2026-03-26 19:52:31 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-website-builder-1-c4i0.onrender.com",
     credentials: true,
   }),
 );
